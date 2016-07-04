082e7b161f5879eedf93714c992b9efd9ed4e843
Jonathan Rothwell <Jonathan.Rothwell@zuhlke.com>
2016-07-04 19:51:10 +0100

    Initial Commit

---
+++ SonicAsteroids/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SonicAsteroids
+//
+//  Created by Jonathan Rothwell on 04/07/2016.
+//  Copyright © 2016 Zuhlke UK. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
